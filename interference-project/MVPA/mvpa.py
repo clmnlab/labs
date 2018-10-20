812b63bfac15932280c2927f6ce2235ac061c11b
eyshin05 <eyshin05@gmail.com>
2018-10-21 01:30:55 +0900

    make a module for some frequently used core functions

---
+++ interference-project/MVPA/mvpa.py
@@ -0,0 +1,81 @@
+import nilearn.image
+import pandas as pd
+import random
+
+from sklearn.model_selection import KFold
+from sklearn.naive_bayes import GaussianNB
+
+
+def load_rois(file_regex_str):
+    fnames = glob.glob(file_regex_str)
+    fnames.sort()
+
+    labels = []
+    masks = []
+
+    for fname in fnames:
+        masks.append(nilearn.image.load_img(fname))
+        label_name = fname.split('/')[-1].replace('nii.gz', '').replace('.nii', '')
+        labels.append(label_name)
+
+    return labels, masks
+
+
+def get_behavior_data(folder_name, subj, run_number, label, stratified_group=False):
+    
+    def _stratified_group(labels):
+        result = []
+        for i, l in labels.groupby('group').count()['order'].iteritems():
+            split_idx = int(l / 2)
+            if l % 2 == 1:
+                if random.random() > 0.5:
+                    split_idx += 1
+
+            result.extend([1] * split_idx)
+            result.extend([2] * (l-split_idx))
+            
+        return result
+    
+    behav_df = pd.read_csv(folder_name + '%s_behav.csv' % subj, index_col=0)
+    behav_df = behav_df[behav_df['run'] == run_number]
+    
+    labels = behav_df.loc[:, ['run', 'order', 'group']]
+    labels['task_type'] = behav_df['%s_type' % label]
+    
+    if stratified_group is True:
+        labels['group'] = _stratified_group(labels)
+    
+    return labels
+
+
+def load_fmri_image(folder_name, subj, run_number, labels):
+    img = nilearn.image.load_img(folder_name + 'betasLSS.%s.r0%d.nii.gz' % (subj, run_number))
+    img = nilearn.image.index_img(img, labels['order'] - 1)
+    img = nilearn.image.clean_img(img)
+
+    return img
+
+
+def masking_fmri_image(fmri_imgs, mask_img):
+    return nilearn.masking.apply_mask(fmri_imgs, mask_img)
+
+
+def run_searchlight(full_mask, X, y, group, estimator='svc'):
+    if estimator is 'gnb':
+        estimator = GaussianNB()
+        
+    cv = KFold(n_splits=2)
+        
+    searchlight = nilearn.decoding.SearchLight(
+        full_mask,
+        radius=8,
+        estimator=estimator,
+        njobs=4,
+        verbose=False,
+        cv=cv
+    )
+    
+    searchlight.fit(X, y, group)
+    scores = searchlgith.scores_
+    
+    return nilearn.image.new_img_like(full_mask, scores)

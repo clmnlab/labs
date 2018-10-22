3cc08ca8109691d0bea3accb54e05a8e6a7d525d
eyshin05 <eyshin05@gmail.com>
2018-10-22 11:32:50 +0900

    move file MVPA/mvpa.py to mvpa_toolkits.py

--- interference-project/MVPA/20181021_run-searchlight-analysis.py
+++ interference-project/MVPA/20181021_run-searchlight-analysis.py
@@ -2,7 +2,7 @@ import nilearn.image
 import random
 import sys
 
-from mvpa import get_behavior_data, load_fmri_image, run_searchlight
+from ..mvpa_toolkits import get_behavior_data, load_fmri_image, run_searchlight
 
 
 if __name__ == '__main__':

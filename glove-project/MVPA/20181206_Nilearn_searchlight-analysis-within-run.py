8f74ae56e4afe75dcd0081c989c3b00d8731254d
eyshin05 <eyshin05@gmail.com>
2018-12-10 17:10:20 +0900

    update code for using subjects cursor position

--- glove-project/MVPA/20181206_Nilearn_searchlight-analysis-within-run.py
+++ glove-project/MVPA/20181206_Nilearn_searchlight-analysis-within-run.py
@@ -1,10 +1,48 @@
 import nilearn.image
+import pandas as pd
 import random
 import sys
 
 from collections import Counter
 from clmnlab_libs.mvpa_toolkits import run_searchlight, get_full_mask, load_5d_fmri_image, average_N_in_4d_image
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+
+
+target_ids = None
+
+
+def get_label_list_and_chance_level():
+    if label == 'path':
+        # all the 12 paths is repeated
+        return list(range(1, 13)) * 8, 1/8
+
+    elif label == 'pos':
+        global target_ids
+
+        if target_ids is None:
+            # read behav file - all the same within subjects
+            with open(behav_dir + 'targetID.txt', 'r') as f:
+                lines = f.readlines()
+
+            # use only 2 ~ 97 lines - first line is dummy, all the sessions has same order
+            target_ids = [int(l.strip()) for l in lines[1:97]]
+
+            # assertion error if all the classes is not the same
+            assert set(Counter(target_ids).values()) == {24}
+
+        return target_ids, 1 / 4
+
+    elif label == 'real-pos':
+        # read behav files
+        label_df = pd.read_csv(behav_dir + '%s_compact.tsv' % subj, delimiter='\t')
+
+        # indexing by runs
+        lines = list(label_df['quadrants'].astype(int))[(run-1)*97+1:run*97]
+
+        return lines, 1 / 4
+
+    else:
+        raise ValueError('!! %s is unknown label name' % label)
 
 
 if __name__ == '__main__':
@@ -18,13 +56,13 @@ if __name__ == '__main__':
         for argv in sys.argv[1:]:
             try:
                 opt, value = argv.split('=')
-                if opt == 'label' and value in {'pos', 'path'}:
+                if opt == 'label' and value in {'pos', 'path', 'real-pos'}:
                     label = value
                 else:
                     raise ValueError
             except ValueError:
                 raise ValueError('Use these options:\n'
-                                 + '  label=label name (pos or path)')
+                                 + '  label=label name (pos, path or real-pos)')
 
     # initialize variables
     data_dir = '/clmnlab/GA/MVPA/TENT_pb02/data/'
@@ -51,30 +89,6 @@ if __name__ == '__main__':
     else:
         raise ValueError('!! %s is unknown estimator name' % estimator_name)
 
-    # label initialize
-    if label == 'path':
-        # all the 12 paths is repeated
-        labels = list(range(1, 13)) * 8
-
-        # set chance level
-        chance_level = 1 / 12
-
-    elif label == 'pos':
-        # read behav file - all the same within subjects
-        with open(behav_dir + 'targetID.txt', 'r') as file:
-            labels = file.readlines()
-
-        # use only 2 ~ 97 lines - first line is dummy, all the sessions has same order
-        labels = [int(l.strip()) for l in labels[1:97]]
-
-        # set chance level
-        chance_level = 1 / 4
-
-        # assertion error if all the classes is not the same
-        assert set(Counter(labels).values()) == {24}
-    else:
-        raise ValueError('!! %s is unknown label name' % label)
-
     mask_img = get_full_mask('/clmnlab/GA/MVPA/fullmask_GAGB/', 'full_mask.GAGB01to19.nii.gz')
 
     for subj in subj_list:
@@ -86,7 +100,7 @@ if __name__ == '__main__':
             img = nilearn.image.index_img(img, range(1, 97))
 
             X = img
-            y = labels
+            y, chance_level = get_label_list_and_chance_level()
 
             searchlight_img = run_searchlight(mask_img, X, y, group,
                                               group_k=8, radius=radius, estimator=estimator, chance_level=chance_level)
